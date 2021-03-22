175c4f8f64866e9e783075c5dc3ae59a6fe5ed5f
yoonkh0204 <79055375+yoonkh0204@users.noreply.github.com>
2021-03-22 11:10:24 +0900

    Add files via upload

---
+++ 0322/16DivideByZero.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Basic
+{
+    class DivideByZero
+    {
+        static void Main(string[] args)
+        {
+            int x = 10, y = 0;
+            try
+            {
+                Console.WriteLine(x / y);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
