c4a114fae7f676b5da44f907719dfff7036f3d42
yoonkh0204 <79055375+yoonkh0204@users.noreply.github.com>
2021-05-03 10:56:46 +0900

    Add files via upload

---
+++ 0503/A098Program.cs
@@ -0,0 +1,29 @@
+﻿using Stack;
+using System;
+
+namespace A98_StackImplementation
+{
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack<int> stack = new MyStack<int>();
+            Random r = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                int val = r.Next(100);
+                stack.Push(val);
+                Console.WriteLine("Push(" + val + ") ");
+                //Console.WriteLine(" top = " + stack.top);
+            }
+
+            Console.WriteLine();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Pop() = " + stack.Pop());
+            }
+        }
+    }
+}
+
---
+++ 0503/A100Program.cs
@@ -0,0 +1,21 @@
+﻿using A100_QueueImplementation;
+using System;
+
+namespace A100_QueueImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            MyQueue<float> que = new MyQueue<float>();
+            for (int i = 0; i < 5; i++)
+                que.EnQueue(new Node<float>(r.Next(100) / 100.0F));
+            que.Print();
+            for (int i = 0; i < 3; i++)
+                Console.WriteLine("DeQueue: {0}", que.DeQueue());
+            que.Print();
+        }
+    }
+}
+
